2d13d8d4a86c759e8591b6070036e220c5fb61e4
Summer <summer.d.howard@gmail.com>
2019-03-24 08:54:01 -0500

    works

--- src/App.js
+++ src/App.js
@@ -1,57 +1,54 @@
 import React, { Component } from 'react';
 import './App.css';
-import EventEmitter from 'EventEmitter';
 
-import Grid from './grid.jsx';
+import SquareWithFormatting from './squareWithFormatting.jsx';
 
-var events = new EventEmitter();
+class App extends Component {
+    constructor(props) {
+        super(props);
 
-class Holder extends Component {
-    state = { value: 'BB' };
+        this.state = {
+            internalState: false
+        };
+
+        // This binding is necessary to make `this` work in the callback
+        this.onMenuItemClicked = this.onMenuItemClicked.bind(this);
+    }
+
+    onMenuItemClicked(e) {
+        e.preventDefault();
+        this.setState({ internalState: !this.state.internalState });
+    }
+
     render() {
-        var cardArray = [
-            { kanji: '白', hiragana: 'しろ' },
-            { kanji: ' 黒 ', hiragana: 'くろ ' },
-            { kanji: ' 金色 ', hiragana: ' きんいろ' }
-        ];
-
-        var display = [];
-        for (var i = 0; i < cardArray.length; i++) {
-            display.push(<div> {cardArray[i].kanji}</div>);
-        }
-
-        console.log('length: ' + cardArray.length);
-
         return (
             <div>
-                {this.state.value} - {display}
+                <div>
+                    <div>Click to change div</div>
+                    <br />
+                    <button
+                        className="btn btn-primary"
+                        onClick={this.onMenuItemClicked}
+                    >
+                        Click
+                    </button>
+                </div>
+                <br />
+
+                <div className="App">
+                    <Grid internalState={this.state.internalState} />
+                </div>
             </div>
         );
     }
 }
 
-class App extends Component {
-    render() {
-        return (
-            <div>
-                <div>
-                    <div>Dragons default to small eyes and red skin</div>
-                    <br />
-                    <button className="btn btn-primary">
-                        Paternal - EE gene (big eyes)
-                    </button>
-                    <br />
-                    <button className="btn btn-primary">
-                        Maternal - BB gene (blue skin)
-                    </button>
-                </div>
-                <br />
-                <div className="App">
-                    <Grid />
-                </div>
-            </div>
-        );
-    }
+function Grid({ internalState }) {
+    return (
+        <div>
+            <SquareWithFormatting internalState={internalState} />
+        </div>
+    );
 }
 
 export default App;
--- src/squareWithFormatting.jsx
+++ src/squareWithFormatting.jsx
@@ -1,28 +1,21 @@
 import React, { Component } from 'react';
-import EventEmitter from 'EventEmitter';
-
-import Square from './square.jsx';
-
-var events = new EventEmitter();
 
 export default class SquareWithFormatting extends Component {
-    state = {};
-
-    simulateClick(e) {
-        //e.click();
-
-        console.log('clicked??');
-        // inside Component1
-        events.emit('change-state', { value: 'YY' });
-    }
-
     render() {
         return (
             <div className="col-sm-4">
-                <div onClick={this.simulateClick}>
-                    <Square />
-                </div>
+                <span className="badge badge-secondary">
+                    {this.formatGene(this.props.internalState)}
+                </span>
             </div>
         );
+    }
+
+    formatGene(internalState) {
+        if (internalState === true) {
+            return 'True';
+        } else {
+            return 'False';
+        }
     }
 }
